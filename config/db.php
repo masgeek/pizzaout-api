0d3bcbe2030dbc22b99c3c809037d55180ce35d8
Sammy Barasa <barsamms@gmail.com>
2017-10-08 12:35:12 +0300

    db update

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;port=3306;dbname=tsobucok_spa', // MySQL, MariaDB
+    'dsn' => 'mysql:host=localhost;port=3306;dbname=tsobucok_pizza', // MySQL, MariaDB
     //'dsn' => 'sqlite:/path/to/database/file', // SQLite
     //'dsn' => 'pgsql:host=localhost;port=5432;dbname=mydatabase', // PostgreSQL
     //'dsn' => 'cubrid:dbname=demodb;host=localhost;port=33000', // CUBRID
@@ -10,7 +10,7 @@ return [
     //'dsn' => 'mssql:host=localhost;dbname=mydatabase', // MS SQL Server, mssql driver
     //'dsn' => 'oci:dbname=//localhost:1521/mydatabase', // Oracle
     //'tablePrefix'=>'tb_',
-    'username' => 'tsobucok_spa',
+    'username' => 'tsobucok_pizza',
     'password' => 'Cyberhopper123',
     'charset' => 'utf8',
 ];
