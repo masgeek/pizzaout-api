cb9f42db249304ad881b8909cc7c2d36c79ac02f
Sammy Barasa <barsamms@gmail.com>
2017-10-09 15:26:31 +0300

    Updates

--- api/modules/v1/models/MENU_ITEM_MODEL.php
+++ api/modules/v1/models/MENU_ITEM_MODEL.php
@@ -14,4 +14,15 @@ use app\models\MenuItem;
 class MENU_ITEM_MODEL extends MenuItem
 {
 
+    public function fields()
+    {
+        $fields = parent::fields();
+
+        $fields['CAT_NAME'] = function ($model) {
+            /* @var $model MENU_ITEM_MODEL */
+
+            return $model->mENUCAT->MENU_CAT_NAME;
+        };
+        return $fields;
+    }
 }
