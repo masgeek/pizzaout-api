405e03ef85118441b761cee9d5b6011f976940a2
Sammy Barasa <barsamms@gmail.com>
2017-11-09 18:14:50 +0300

    Updated teh workflow

--- model_extended/CUSTOMER_ORDERS.php
+++ model_extended/CUSTOMER_ORDERS.php
@@ -16,74 +16,75 @@ use app\models\OrderTracking;
 class CUSTOMER_ORDERS extends CustomerOrder
 {
 
-	public $COMMENTS;
+    public $COMMENTS;
 
-	public function attributeLabels()
-	{
-		$labels = parent::attributeLabels();
-		$labels['ADDRESS_ID'] = 'Delivery Address';
-		$labels['USER_ID'] = 'Customer';
-		$labels['KITCHEN_ID'] = 'Assign Kitchen';
-		$labels['CHEF_ID'] = 'Assign Chef';
-		$labels['RIDER_ID'] = 'Assign Rider';
-		$labels['NOTES'] = 'Notes';
-		$labels['ORDER_PRICE'] = 'Total';
-		$labels['PAYMENT_METHOD'] = 'Payment';
-		$labels['ORDER_STATUS'] = 'Status';
-		$labels['ORDER_ID'] = 'Order ID #';
+    public function attributeLabels()
+    {
+        $labels = parent::attributeLabels();
+        $labels['ADDRESS_ID'] = 'Delivery Address';
+        $labels['USER_ID'] = 'Customer';
+        $labels['KITCHEN_ID'] = 'Assign Kitchen';
+        $labels['CHEF_ID'] = 'Assign Chef';
+        $labels['RIDER_ID'] = 'Assign Rider';
+        $labels['NOTES'] = 'Notes';
+        $labels['ORDER_PRICE'] = 'Total';
+        $labels['PAYMENT_METHOD'] = 'Payment';
+        $labels['ORDER_STATUS'] = 'Status';
+        $labels['ORDER_ID'] = 'Order ID #';
 
-		return $labels;
-	}
+        return $labels;
+    }
 
 
-	public function rules()
-	{
-		$rules = parent::rules();
+    public function rules()
+    {
+        $rules = parent::rules();
 
         $rules[] = [['ORDER_STATUS'], 'required', 'on' => [
-			APP_UTILS::SCENARIO_ALLOCATE_KITCHEN,
-			APP_UTILS::SCENARIO_CONFIRM_ORDER,
-			APP_UTILS::SCENARIO_ASSIGN_CHEF,
-			APP_UTILS::SCENARIO_PREPARE_ORDER,
-			APP_UTILS::SCENARIO_ORDER_READY,
-			APP_UTILS::SCENARIO_ASSIGN_RIDER,
-		]];
+            APP_UTILS::SCENARIO_ALLOCATE_KITCHEN,
+            APP_UTILS::SCENARIO_CONFIRM_ORDER,
+            APP_UTILS::SCENARIO_ASSIGN_CHEF,
+            APP_UTILS::SCENARIO_PREPARE_ORDER,
+            APP_UTILS::SCENARIO_ORDER_READY,
+            APP_UTILS::SCENARIO_ASSIGN_RIDER,
+        ]];
         $rules[] = [['KITCHEN_ID'], 'required', 'on' => [APP_UTILS::SCENARIO_ALLOCATE_KITCHEN,]];
         //$rules[] = [['COMMENTS'], 'required', 'on' => [APP_UTILS::SCENARIO_CONFIRM_ORDER,]];
-		$rules[] = [['CHEF_ID'], 'required', 'on' => [APP_UTILS::SCENARIO_ASSIGN_CHEF,]];
-		return $rules;
-	}
+        $rules[] = [['CHEF_ID'], 'required', 'on' => [APP_UTILS::SCENARIO_ASSIGN_CHEF,]];
+        $rules[] = [['RIDER_ID'], 'required', 'on' => [APP_UTILS::SCENARIO_ASSIGN_RIDER,]];
+        return $rules;
+    }
 
-	public function beforeSave($insert)
-	{
-		$date = APP_UTILS::GetCurrentDateTime();
-		if (parent::beforeSave($insert)) {
-			if ($this->isNewRecord) {
-				$this->CREATED_AT = $date;
-			}
-			$this->UPDATED_AT = $date;
-			return true;
-		}
-		return false;
-	}
+    public function beforeSave($insert)
+    {
+        $date = APP_UTILS::GetCurrentDateTime();
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->CREATED_AT = $date;
+            }
+            $this->UPDATED_AT = $date;
+            return true;
+        }
+        return false;
+    }
 
-	public function afterSave($insert, $changedAttributes)
-	{
-		parent::afterSave($insert, $changedAttributes);
-		//lets add to tracking table
-		$tracker = new STATUS_TRACKING_MODEL();
-		$tracker->ORDER_ID = $this->ORDER_ID;
-		$tracker->STATUS = $this->ORDER_STATUS;
-		$tracker->COMMENTS = $this->COMMENTS;
-		$tracker->TRACKING_DATE = APP_UTILS::GetCurrentDateTime();
-		$tracker->save();
-	}
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+        //lets add to tracking table
+        $tracker = new STATUS_TRACKING_MODEL();
+        $tracker->ORDER_ID = $this->ORDER_ID;
+        $tracker->STATUS = $this->ORDER_STATUS;
+        $tracker->COMMENTS = $this->COMMENTS;
+        $tracker->TRACKING_DATE = APP_UTILS::GetCurrentDateTime();
+        $tracker->save();
+    }
 
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getOrderTrackings()
-	{
-		return $this->hasMany(OrderTracking::className(), ['ORDER_ID' => 'ORDER_ID'])->orderBy(['TRACKING_DATE' => SORT_DESC]);
-	}
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrderTrackings()
+    {
+        return $this->hasMany(OrderTracking::className(), ['ORDER_ID' => 'ORDER_ID'])->orderBy(['TRACKING_DATE' => SORT_DESC]);
+    }
 }
