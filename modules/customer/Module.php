187e41440ac954d3f3b6ad316651c70c19a66a5c
Sammy Barasa <barsamms@gmail.com>
2017-11-10 09:34:40 +0300

    Added customer layout

--- modules/customer/Module.php
+++ modules/customer/Module.php
@@ -22,6 +22,7 @@ class Module extends \yii\base\Module
         // custom initialization code goes here
         //set custom theme for the customer module
         //$this->layoutPath = \Yii::getAlias('@app/themes/omnifood/layouts/');
+        $this->layoutPath = \Yii::getAlias('@app/themes/porto_theme/layouts/');
         $this->layout = 'customer_layout';
     }
 
