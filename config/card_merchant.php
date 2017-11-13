c3a4b1dbd68f4359fb811caa8ca8341d3134a85e
Sammy Barasa <barsamms@gmail.com>
2017-11-13 13:09:30 +0300

    Added card merchant config url

---
+++ config/card_merchant.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'instName' => 'University of Nairobi',
+    //base url for the links generated in the email
+    'returnURL' => 'http://pizza.tsobu.co.ke/',
+    'baseURL' => 'https://migs-mtf.mastercard.com.au',
+    //Payment gateways information
+    'merchantcodeKES' => 'CA1B7397',
+    'merchantidKES' => '00000123',
+    'tokenKES' => '1E7941830AC474594028E6FEFCFB41AC',
+
+    //USD GATEWAY
+    'merchantcodeUSD' => 'D17ED546',
+    'merchantidUSD' => '00000129',
+    'tokenUSD' => '44F5091883BFDF85F158EA0A37D56902',
+    'gatewayURL' => 'https://migs-mtf.mastercard.com.au/vpcpay?',
+    'vpcVersion' => '1',
+];
