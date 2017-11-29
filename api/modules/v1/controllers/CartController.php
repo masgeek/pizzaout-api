0c4043278b104d4fd75637d537c01ef27c9a9368
Masgeek <barsamms@gmail.com>
2017-11-29 16:08:19 +0300

    Removal of redundant classes and namspces

--- api/modules/v1/controllers/CartController.php
+++ api/modules/v1/controllers/CartController.php
@@ -10,11 +10,8 @@ namespace app\api\modules\v1\controllers;
 
 use app\api\modules\v1\models\CART_MODEL;
 use app\api\modules\v1\models\MENU_ITEM_MODEL;
-use app\api\modules\v1\models\OFFERED_SERVICE_MODEL;
-use app\api\modules\v1\models\RESERVED_SERVICE_MODEL;
-use app\api\modules\v1\models\SALON_MODEL;
-use app\api\modules\v1\models\STAFF_MODEL;
 use yii\rest\ActiveController;
+use yii\web\ForbiddenHttpException;
 
 
 class CartController extends ActiveController
