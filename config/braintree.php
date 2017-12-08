2c2c9c510bb1b0109d7d26b7bfa358d7f5b13cc1
Masgeek <barsamms@gmail.com>
2017-12-08 12:28:02 +0300

    Disabled braintree component momentarily
    Signed-off-by: Masgeek <barsamms@gmail.com>

--- config/braintree.php
+++ config/braintree.php
@@ -1,7 +1,9 @@
 <?php
-return [
+/*return [
     'class' => 'tuyakhov\braintree\Braintree',
     'merchantId' => 't6ygyzrt59f2m7mr',
     'publicKey' => 'vh8ctdxv4fqwgtpt',
     'privateKey' => '2bc24b7befcaca84f632ea9cc78806dd',
-];
+];*/
+
+return [];
