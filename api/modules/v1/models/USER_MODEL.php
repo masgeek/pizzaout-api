cec7fc488cf2ab7c1ea9f1b584017c56600010a4
Masgeek <barsamms@gmail.com>
2017-12-14 14:54:10 +0300

    update on models and rider assignement

--- api/modules/v1/models/USER_MODEL.php
+++ api/modules/v1/models/USER_MODEL.php
@@ -21,62 +21,71 @@ User_Type
 
  */
 use app\helpers\APP_UTILS;
+use app\model_extended\RIDER_MODEL;
 use app\models\Users;
 use app\models\UserType;
 use yii\db\Expression;
 
 class USER_MODEL extends Users
 {
-	public $CHANGE_PASSWORD;
+    public $CHANGE_PASSWORD;
 
 
-	public function scenarios()
-	{
-		$scenarios = parent::scenarios();
-		$scenarios[APP_UTILS::SCENARIO_CREATE] = ['SURNAME', 'OTHER_NAMES', 'MOBILE', 'EMAIL', 'LOCATION_ID', 'USER_NAME', 'PASSWORD', 'USER_TYPE'];
-		$scenarios[APP_UTILS::SCENARIO_UPDATE] = ['SURNAME', 'OTHER_NAMES', 'MOBILE', 'EMAIL', 'LOCATION_ID', 'USER_NAME', 'PASSWORD', 'USER_TYPE'];
+    public function scenarios()
+    {
+        $scenarios = parent::scenarios();
+        $scenarios[APP_UTILS::SCENARIO_CREATE] = ['SURNAME', 'OTHER_NAMES', 'MOBILE', 'EMAIL', 'LOCATION_ID', 'USER_NAME', 'PASSWORD', 'USER_TYPE'];
+        $scenarios[APP_UTILS::SCENARIO_UPDATE] = ['SURNAME', 'OTHER_NAMES', 'MOBILE', 'EMAIL', 'LOCATION_ID', 'USER_NAME', 'PASSWORD', 'USER_TYPE'];
 
-		return $scenarios;
-	}
+        return $scenarios;
+    }
 
-	public function rules()
-	{
-		$rules = parent::rules();
+    public function rules()
+    {
+        $rules = parent::rules();
 
-		$rules[] = [['EMAIL'], 'unique', 'on' => [APP_UTILS::SCENARIO_CREATE, APP_UTILS::SCENARIO_UPDATE]];
-		$rules[] = [['PASSWORD'], 'string', 'min' => 3, 'on' => [APP_UTILS::SCENARIO_CREATE, APP_UTILS::SCENARIO_UPDATE]];
-		return $rules;
-	}
+        $rules[] = [['EMAIL'], 'unique', 'on' => [APP_UTILS::SCENARIO_CREATE, APP_UTILS::SCENARIO_UPDATE]];
+        $rules[] = [['PASSWORD'], 'string', 'min' => 3, 'on' => [APP_UTILS::SCENARIO_CREATE, APP_UTILS::SCENARIO_UPDATE]];
+        return $rules;
+    }
 
-	public function beforeSave($insert)
-	{
-		if (parent::beforeSave($insert)) {
-			if ($this->isNewRecord) {
-				$this->DATE_REGISTERED = APP_UTILS::GetCurrentDateTime();
-			}
-			$this->LAST_UPDATED =APP_UTILS::GetCurrentDateTime();
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->DATE_REGISTERED = APP_UTILS::GetCurrentDateTime();
+            }
+            $this->LAST_UPDATED = APP_UTILS::GetCurrentDateTime();
 
-			return true;
-		}
-		return false;
-	}
+            return true;
+        }
+        return false;
+    }
 
-	public function attributeLabels()
-	{
-		$labels = parent::attributeLabels();
+    public function attributeLabels()
+    {
+        $labels = parent::attributeLabels();
 
-		return $labels;
-	}
+        return $labels;
+    }
 
-	public function fields()
-	{
-		$fields = parent::fields();
+    public function fields()
+    {
+        $fields = parent::fields();
 
-		$fields['USER_TYPE'] = function ($model) {
-			/* @var $model USER_MODEL */
-			return UserType::findOne($model->USER_TYPE)->USER_TYPE_NAME;
-		};
-		//unset($fields['PASSWORD']); //remove the password field
-		return $fields;
-	}
+        $fields['USER_TYPE'] = function ($model) {
+            /* @var $model USER_MODEL */
+            return UserType::findOne($model->USER_TYPE)->USER_TYPE_NAME;
+        };
+
+        $fields['RIDER_ID'] = function ($model) {
+            /* @var $model USER_MODEL */
+            $rider = RIDER_MODEL::findOne(['USER_ID' => $model->USER_ID]);
+            return $rider != null ? $rider->RIDER_ID : 0;
+        };
+        unset($fields['PASSWORD']); //remove the password field
+
+        ksort($fields);
+        return $fields;
+    }
 }
