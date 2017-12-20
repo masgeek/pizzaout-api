fbbe29cd6eba2a861cb6ff064137a4bbd0a42b8e
Masgeek <barsamms@gmail.com>
2017-12-20 10:19:31 +0300

    Added requirements page

--- config/formatter.php
+++ config/formatter.php
@@ -7,7 +7,7 @@ return
         //'dateFormat' => 'dd.MM.yyyy',
         'decimalSeparator' => '.',
         'thousandSeparator' => ',',
-        //'timeZone' => $timezone, //default time zones and format
+        'timeZone' => $timezone, //default time zones and format
         //'currencyCode' => 'USD',
         'currencyCode' => 'KES',
         'nullDisplay' => '0'
