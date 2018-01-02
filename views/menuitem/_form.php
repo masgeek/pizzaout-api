411d7dc1304144083df8817833ca8abd26c1789b
Sammy Barasa <barsamms@gmail.com>
2018-01-02 20:11:37 +0300

    Adde dasani water images
    Signed-off-by: Sammy Barasa <barsamms@gmail.com>

--- views/menuitem/_form.php
+++ views/menuitem/_form.php
@@ -26,16 +26,16 @@ $model->MENU_ITEM_IMAGE = '1.jpg';
     <?php $form = ActiveForm::begin(); ?>
 
     <div class="row">
-        <?= $form->field($model, 'MENU_CAT_ID',['template' => $field_template])->dropDownList(\app\model_extended\MENU_CATEGORY::GetMenuCategories()) ?>
+        <?= $form->field($model, 'MENU_CAT_ID', ['template' => $field_template])->dropDownList(\app\model_extended\MENU_CATEGORY::GetMenuCategories()) ?>
     </div>
     <div class="row">
-        <?= $form->field($model, 'MENU_ITEM_NAME',['template' => $field_template])->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'MENU_ITEM_NAME', ['template' => $field_template])->textInput(['maxlength' => true]) ?>
     </div>
     <div class="row">
-        <?= $form->field($model, 'MENU_ITEM_DESC',['template' => $field_template])->textarea(['rows' => 6]) ?>
+        <?= $form->field($model, 'MENU_ITEM_DESC', ['template' => $field_template])->textarea(['rows' => 6]) ?>
     </div>
     <div class="row">
-        <?= $form->field($model, 'MENU_ITEM_IMAGE')->hiddenInput(['maxlength' => true])->label(false) ?>
+        <?= $form->field($model, 'MENU_ITEM_IMAGE')->textInput(['maxlength' => true])->hint("Please upload an image") ?>
     </div>
     <div class="row">
         <?= $form->field($model, 'HOT_DEAL')->checkbox() ?>
@@ -44,7 +44,7 @@ $model->MENU_ITEM_IMAGE = '1.jpg';
         <?= $form->field($model, 'VEGETARIAN')->checkbox() ?>
     </div>
     <div class="row">
-        <?= $form->field($model, 'MAX_QTY',['template' => $field_template])->textInput() ?>
+        <?= $form->field($model, 'MAX_QTY', ['template' => $field_template])->textInput() ?>
     </div>
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
