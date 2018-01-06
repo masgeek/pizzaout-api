f2f3f8ffc376ee2fb6b9cb916c1369f71fb3e81e
Sammy Barasa <barsamms@gmail.com>
2018-01-06 12:59:22 +0300

    temporarily disabled header checking
    Signed-off-by: Sammy Barasa <barsamms@gmail.com>

--- api/modules/v1/controllers/MenuitemController.php
+++ api/modules/v1/controllers/MenuitemController.php
@@ -78,6 +78,8 @@ class MenuitemController extends ActiveController
         $this->_apiToken = Yii::$app->request->headers->get("api_token", null);
         $this->_userID = Yii::$app->request->headers->get("user_id", null);
         //return Yii::$app->request->headers;
+
+        return $this->_userID;
         $this->checkAccess('cat-item');
         return MENU_ITEM_MODEL::find()
             ->where(['MENU_CAT_ID' => $menu_cat_id])
