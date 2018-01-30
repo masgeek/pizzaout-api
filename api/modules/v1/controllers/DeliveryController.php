c186df4b70aa2ab2c483996b124852d50bb57a0c
Sammy Barasa <barsamms@gmail.com>
2018-01-30 18:34:50 +0300

    coreectedd chef list action call
    Signed-off-by: Sammy Barasa <barsamms@gmail.com>

--- api/modules/v1/controllers/DeliveryController.php
+++ api/modules/v1/controllers/DeliveryController.php
@@ -26,7 +26,7 @@ class DeliveryController extends ActiveController
         $actions = parent::actions();
 
         // disable the "delete" and "create" actions
-        unset($actions['delete'], $actions['create']);
+        //unset($actions['delete'], $actions['create']);
 
         // customize the data provider preparation with the "prepareDataProvider()" method
         $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
