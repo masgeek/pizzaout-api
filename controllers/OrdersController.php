1c139a095826b9d0f8ee6b930d820367bc3c5fab
Sammy Barasa <barsamms@gmail.com>
2018-03-23 23:12:24 +0300

    Testing email

--- controllers/OrdersController.php
+++ controllers/OrdersController.php
@@ -169,6 +169,17 @@ class OrdersController extends Controller
     }
 
 
+    public function actionTestMail($id)
+    {
+
+        $this->view->title = "Order #{$id}";
+        $model = $this->findModel($id);
+        $model->scenario = APP_UTILS::SCENARIO_CONFIRM_ORDER;
+
+        return APP_UTILS::SendOrderEmailWithReceipt($model->uSER, $model->ORDER_ID, $model->oRDERSTATUS->STATUS_NAME, true);
+
+    }
+
     /**
      * Updates an existing CUSTOMER_ORDERS model.
      * If update is successful, the browser will be redirected to the 'view' page.
